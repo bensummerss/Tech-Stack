ffdd01fafd3c7e5717055bb115b6d2a7c583055c
bensummrs <bensummersonline@gmail.com>
2023-12-29 00:13:09 +0000

    add and remove render fragments functionality, added form model class

---
+++ src/techstack.team.builder.blazor.web/techstack.team.builder.blazor.web/Models/FormModel.cs
@@ -0,0 +1,12 @@
+﻿namespace techstack.team.builder.blazor.web.Models
+{
+    public class FormModel
+    {
+        public List<TeamBuilderFormItem> TeamBuilderFormItems { get; set; } = new List<TeamBuilderFormItem>();
+    }
+
+    public class TeamBuilderFormItem
+    {
+        public string? SelectedValues { get; set; }
+    }
+}
