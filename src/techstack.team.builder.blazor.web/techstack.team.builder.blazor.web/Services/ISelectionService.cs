8bea4b89dd2745ebcad88e348f74e9324f4f2f88
bensummrs <bensummersonline@gmail.com>
2023-12-29 15:07:05 +0000

    reading selected values with selection service

---
+++ src/techstack.team.builder.blazor.web/techstack.team.builder.blazor.web/Services/ISelectionService.cs
@@ -0,0 +1,11 @@
+﻿namespace techstack.team.builder.blazor.web.Services
+{
+    public interface ISelectionService
+    {
+        event Action OnSelectionChanged;
+        string SelectedRole { get; set; }
+        string SelectedSkillset { get; set; }
+        string SelectedSkillLevel { get; set; }
+        int SelectedQuantity { get; set; }
+    }
+}
---
+++ src/techstack.team.builder.blazor.web/techstack.team.builder.blazor.web/Services/SelectionService.cs
@@ -0,0 +1,51 @@
+﻿namespace techstack.team.builder.blazor.web.Services
+{
+    public class SelectionService : ISelectionService
+    {
+        public event Action OnSelectionChanged;
+        public string _selectedRole { get; set; }
+        public string SelectedRole
+        {
+            get => _selectedRole;
+            set
+            {
+                _selectedRole = value;
+                NotifySelectionChanged();
+            }
+        }
+        public string _selectedSkillset { get; set; }
+        public string SelectedSkillset
+        {
+            get => _selectedSkillset;
+            set
+            {
+                _selectedSkillset = value;
+                NotifySelectionChanged();
+            }
+        }
+        public string _selectedSkillLevel { get; set; }
+        public string SelectedSkillLevel
+        {
+            get => _selectedSkillLevel;
+            set
+            {
+                _selectedSkillLevel = value;
+                NotifySelectionChanged();
+            }
+        }
+        public int _selectedQuantity { get; set; }
+        public int SelectedQuantity
+        {
+            get => _selectedQuantity;
+            set
+            {
+                _selectedQuantity = value;
+                NotifySelectionChanged();
+            }
+        }
+        private void NotifySelectionChanged()
+        {
+            OnSelectionChanged?.Invoke();
+        }
+    }
+}
